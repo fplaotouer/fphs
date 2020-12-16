6013e6a9f05fb5c1751bd5f4d90192bd66ce144d
Pangz <fpen9@outlook.com>
2020-12-16 22:36:04 +0800

    Add type alias `Mutex`

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
-    ( someFunc
+    ( runDemo
     ) where
+
+import Mutex
 
 import System.IO
 import Control.Concurrent
@@ -11,27 +13,26 @@ getChanWhileJust :: Chan (Maybe a) -> IO [a]
 -- getChanWhileJust chan = fmap (catMaybes . takeWhile isJust) (getChanContents chan)
 getChanWhileJust chan = fmap (catMaybes . take 11) (getChanContents chan)
 
-someFunc :: IO ()
-someFunc = do
-  io <- newEmptyMVar
-  putMVar io ()
+runDemo :: IO ()
+runDemo = do
+  mutex <- newMutex
   res <- newChan
-  calc [1..100] res io
+  calc [1..100] res mutex
   getChanWhileJust res >>= putStrLn . show . sum
 
-sumUp :: (Show a, Num a) => MVar [a] -> Chan (Maybe a) -> MVar () -> IO ()
-sumUp input res io = do
+sumUp :: (Show a, Num a) => MVar [a] -> Chan (Maybe a) -> Mutex -> IO ()
+sumUp input res mutex = do
   lst <- takeMVar input
-  takeMVar io
+  lock mutex
   tid <- myThreadId
   putStrLn $ show tid ++ ":\t" ++ show lst
-  putMVar io ()
+  unLock mutex
   writeChan res $ Just $ sum lst
 
-calc :: (Show a, Num a) => [a] -> Chan (Maybe a) -> MVar() -> IO ()
+calc :: (Show a, Num a) => [a] -> Chan (Maybe a) -> Mutex -> IO ()
 calc [] res _ = writeChan res Nothing
-calc xs res io = do
+calc xs res mutex = do
   buf <- newEmptyMVar
   putMVar buf . take 10 $ xs
-  forkIO $ sumUp buf res io
-  calc (drop 10 $ xs) res io
+  forkIO $ sumUp buf res mutex
+  calc (drop 10 $ xs) res mutex
---
+++ src/Mutex.hs
@@ -0,0 +1,14 @@
+module Mutex where
+
+import Control.Concurrent.MVar (MVar, newMVar, takeMVar, putMVar)
+
+type Mutex = MVar ()
+
+newMutex :: IO Mutex
+newMutex = newMVar ()
+
+lock :: Mutex -> IO ()
+lock = takeMVar
+
+unLock :: Mutex -> IO ()
+unLock = flip putMVar ()
